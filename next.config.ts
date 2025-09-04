368244bcade0af350f513159a1a18a5932967575
nevin.e <nevin.e@inapp.com>
2025-09-04 10:45:31 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  disable: isDev,
+  skipWaiting: true
+})
+
+export default pwaConfig(nextConfig);
