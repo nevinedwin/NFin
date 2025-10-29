ab4396fbff8cea5fdab78bafc6cf355ad1a1f455
nevin.e <nevin.e@inapp.com>
2025-10-29 22:42:08 +0530

    updated code

--- app/clientComponent.tsx
+++ app/clientComponent.tsx
@@ -1,17 +1,30 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Splashscreen from './components/splashscreen';
 
 type ClientComponentType = React.PropsWithChildren;
 
 const ClientComponent: React.FC<ClientComponentType> = ({ children }) => {
-  const [showSplash, setShowSplash] = useState(true);
+  const [showSplash, setShowSplash] = useState(false);
+
+  useEffect(() => {
+    const splashShown = localStorage.getItem('splashShown');
+
+    if (!splashShown) {
+      setShowSplash(true);
+    }
+  }, []);
+
+  const handleFinish = () => {
+    localStorage.setItem('splashShown', 'true');
+    setShowSplash(false);
+  };
 
   return (
     <div>
       {showSplash ? (
-        <Splashscreen onFinish={() => setShowSplash(false)} />
+        <Splashscreen onFinish={handleFinish} />
       ) : (
         <div className="fade-in h-screen flex flex-col items-center justify-center bg-color-background">
           {children}
