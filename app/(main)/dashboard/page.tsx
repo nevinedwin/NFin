c9e011dc9f6ba5b63b4794194486966428d21dda
nevinedwin <nevinedwin100@gmail.com>
2026-05-01 18:16:52 +0530

    'deploy_dev'

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,9 +1,9 @@
 'use server';
 
 
-import OverviewServer from "@/components/dashboard/overviewServer";
 import Dashboard from "./dashboard";
-import CategoryOverviewServer from "@/components/dashboard/catgeoryOverviewServer";
+// import OverviewServer from "@/components/dashboard/overviewServer";
+// import CategoryOverviewServer from "@/components/dashboard/catgeoryOverviewServer";
 
 export default async function DashboardPage() {
     // return <Dashboard overview={<OverviewServer />} categoryOverView={<CategoryOverviewServer />} />;
